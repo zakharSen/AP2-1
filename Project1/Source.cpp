972242227d0fa3b6aa4e62edb5da68f3470b98ac
zakharSen <139217440+zakharSen@users.noreply.github.com>
2023-09-28 14:58:24 +0300

    GoodJob!

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -10,8 +10,9 @@ int main()
 	z1 = 2 * x;
 	z2 = x + x;
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
-	//cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cout << "Good job!"<<endl;
 	cin.get();
 	return 0;
 }
