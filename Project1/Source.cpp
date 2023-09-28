974629b68c68c1c557fc46b78527aed9eeddd344
zakharSen <139217440+zakharSen@users.noreply.github.com>
2023-09-28 15:00:54 +0300

    WellDOne!

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
 	double x; 
-	double z1; 
+	double z5; 
 	double z2; 
 	cout << "x = "; cin >> x;
-	z1 = 2 * x;
+	z5 = 2 * x;
 	z2 = x + x;
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
+	cout << "z1 = " << z5 << endl;
 	cout << "z2 = " << z2 << endl;
 	cout << "Good job!"<<endl;
 	cin.get();
