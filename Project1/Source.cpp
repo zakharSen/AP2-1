05593c8879cf33e944979687f49a83fc588f69e2
zakharSen <139217440+zakharSen@users.noreply.github.com>
2023-09-28 14:41:35 +0300

    FirstCom

---
+++ Project1/Source.cpp
@@ -0,0 +1,17 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+	double x; 
+	double z1; 
+	double z2; 
+	cout << "x = "; cin >> x;
+	z1 = 2 * x;
+	z2 = x + x;
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
